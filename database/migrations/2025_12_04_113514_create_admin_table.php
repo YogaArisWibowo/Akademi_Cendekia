5e85b13ed7841a516e2705ac93808daa13f5927d
YogaArisWibowo <yoga09332@gmail.com>
2025-12-27 09:36:33 +0700

    Update kodingan saya

--- database/migrations/2025_12_04_113514_create_admin_table.php
+++ database/migrations/2025_12_04_113514_create_admin_table.php
@@ -13,17 +13,9 @@ return new class extends Migration
     {
         Schema::create('admin', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-            // Ubah 'user' menjadi 'users'
-            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
-            $table->string('nama');
-            $table->string('username', 50);
-            $table->string('password', 8); // Hati-hati, password hash biasanya butuh 60+ karakter (string default 255)
-=======
             $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
             $table->string('username');
             $table->string('nama');
->>>>>>> e5e5dc46417bbbae78796b7c3b2407d398da6b95
             $table->timestamps();
         });
     }
